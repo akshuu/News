a839dbb6e131e19e0f500310f69de1542ed61739
Akshat Jain <akshuu@gmail.com>
2016-07-29 22:04:57 -0700

    adding fullscreen view to webview with share & Close button

--- app/src/main/java/com/akshatjain/codepath/news/activities/WebViewActivity.java
+++ app/src/main/java/com/akshatjain/codepath/news/activities/WebViewActivity.java
@@ -1,9 +1,17 @@
 package com.akshatjain.codepath.news.activities;
 
+import android.content.Intent;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.akshatjain.codepath.news.R;
 
@@ -14,16 +22,93 @@ public class WebViewActivity extends AppCompatActivity {
 
     @BindView(R.id.webView)
     WebView mWebView;
+
+    @BindView(R.id.btnClose)
+    ImageButton btnClose;
+
+    @BindView(R.id.btnShare)
+    ImageButton btnShare;
+
+    @BindView(R.id.buttonPanel)
+    LinearLayout btnPanel;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
         ButterKnife.bind(this);
+
         mWebView.setWebViewClient(new WebViewClient());
-        String url = getIntent().getStringExtra("URL");
+
+        mWebView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (btnPanel.getVisibility() == View.GONE)
+                        showSystemUI();
+                    else {
+                        goFullImmersive();
+                    }
+                }
+                return false;
+            }
+        });
+        final String url = getIntent().getStringExtra("URL");
         if(url != null){
             mWebView.loadUrl(url);
         }
+
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WebViewActivity.this.finish();
+            }
+        });
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, url);
+                startActivity(Intent.createChooser(sharingIntent,"Share Link via..."));
+            }
+        });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            goFullImmersive();
+        }
+    }
+
+    private void goFullImmersive() {
+        View decorView = getWindow().getDecorView();
+        int visibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(
+                visibility);
+        btnPanel.setVisibility(View.GONE);
+    }
+
+    // This snippet shows the system bars. It does this by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        btnPanel.setVisibility(View.VISIBLE);
     }
 }
